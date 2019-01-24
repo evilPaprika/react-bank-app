48f01da2c5cc587b6cbac8612501dde8f716c0f4
evilpaprika <evilpaprika@localhost.localdomain>
2019-01-24 18:16:24 +0500

    начал делать оплату картой

--- src/components/payment/PayUsingCard.jsx
+++ src/components/payment/PayUsingCard.jsx
@@ -3,7 +3,38 @@ import React, { Component } from "react";
 class PayUsingCard extends Component {
   state = {};
   render() {
-    return <div>PayUsingCard</div>;
+    return (
+      <div className="pay-card">
+        <div className="pay-card__card">
+          <div className="pay-card__card-bg">card</div>
+        </div>
+        <div className="pay-card__other-info">
+          <label htmlFor="amount-of-money">Сумма</label>
+          <input
+            type="number"
+            name="amount of money"
+            id="amount-of-money"
+            placeholder="от 1 000 до 75 000₽"
+          />
+          <label htmlFor="comment">Комментарий</label>
+          <input
+            type="string"
+            name="comment"
+            id="comment"
+            placeholder="До 150 символов"
+          />
+          <label htmlFor="client-email">Ваш email</label>
+          <input
+            type="email"
+            name="email"
+            id="client-email"
+            placeholder="Для квитанций об оплате"
+          />
+          <br />
+          <button type="submit">Заплатить</button>
+        </div>
+      </div>
+    );
   }
 }
 
