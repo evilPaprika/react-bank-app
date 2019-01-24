8df3f4ebfdd1a74f09e5c8c8dc02e2c52a00fa14
evilpaprika <evilpaprika@localhost.localdomain>
2019-01-24 17:00:47 +0500

    починил навигацию

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -3,26 +3,27 @@ import Navbar from "./payment/Navbar";
 import RequestPayment from "./payment/RequestPayment";
 import PayUsingCard from "./payment/PayUsingCard";
 import PayUsingOnlineBank from "./payment/PayUsingOnlineBank";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "../styles/Payment.css";
 
 class Payment extends Component {
   state = {};
   render() {
     return (
-      <div className="box-wrapper">
-        <Navbar />
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className="box-wrapper">
+          <Navbar />
           <Switch>
+            <Redirect exact from="/" to="/pay" />
             <Route
               path="/pay/using_online_bank"
               component={PayUsingOnlineBank}
             />
             <Route path="/request_payment" component={RequestPayment} />
-            <Route path={["/", "/pay/using_card"]} component={PayUsingCard} />
+            <Route path="/pay/using_card" component={PayUsingCard} />
           </Switch>
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/payment/Navbar.jsx
+++ src/components/payment/Navbar.jsx
@@ -7,58 +7,56 @@ class Navbar extends Component {
   };
   render() {
     return (
-      <BrowserRouter className="navbar">
-        <div>
-          <div className="navbar__first-row">
-            <NavLink
-              to="/pay"
-              activeClassName="link-selected"
-              className="navbar__choice"
-            >
-              Заплатить
-            </NavLink>{" "}
-            <NavLink
-              to="/request_payment"
-              activeClassName="link-selected"
-              className="navbar__choice"
-            >
-              Запросить&nbsp;платёж
-            </NavLink>
-          </div>
-          <Route
-            path="/pay"
-            component={() => (
-              <div className="navbar__second-row">
-                <Route
-                  exact
-                  path="/pay"
-                  render={() => (
-                    <Redirect to={"/pay/" + this.state.secondChoice} />
-                  )}
-                />
-                <NavLink
-                  onClick={() => this.setState({ secondChoice: "using_card" })}
-                  to="/pay/using_card"
-                  activeClassName="link-selected"
-                  className="navbar__choice-second navbar__from-card-choice"
-                >
-                  💳&nbsp;С&nbsp;карты&nbsp;любого&nbsp;банка
-                </NavLink>{" "}
-                <NavLink
-                  onClick={() =>
-                    this.setState({ secondChoice: "using_online_bank" })
-                  }
-                  to="/pay/using_online_bank"
-                  activeClassName="link-selected"
-                  className="navbar__choice-second navbar__from-internet-bank-choice"
-                >
-                  💻&nbsp;Из&nbsp;своего&nbsp;интернет&#8209;банка
-                </NavLink>
-              </div>
-            )}
-          />
+      <div className="navbar">
+        <div className="navbar__first-row">
+          <NavLink
+            to="/pay"
+            activeClassName="link-selected"
+            className="navbar__choice"
+          >
+            Заплатить
+          </NavLink>{" "}
+          <NavLink
+            to="/request_payment"
+            activeClassName="link-selected"
+            className="navbar__choice"
+          >
+            Запросить&nbsp;платёж
+          </NavLink>
         </div>
-      </BrowserRouter>
+        <Route
+          path="/pay"
+          component={() => (
+            <div className="navbar__second-row">
+              <Route
+                exact
+                path="/pay"
+                render={() => (
+                  <Redirect to={"/pay/" + this.state.secondChoice} />
+                )}
+              />
+              <NavLink
+                onClick={() => this.setState({ secondChoice: "using_card" })}
+                to="/pay/using_card"
+                activeClassName="link-selected"
+                className="navbar__choice-second navbar__from-card-choice"
+              >
+                💳&nbsp;С&nbsp;карты&nbsp;любого&nbsp;банка
+              </NavLink>{" "}
+              <NavLink
+                onClick={() =>
+                  this.setState({ secondChoice: "using_online_bank" })
+                }
+                to="/pay/using_online_bank"
+                activeClassName="link-selected"
+                className="navbar__choice-second navbar__from-internet-bank-choice"
+              >
+                💻&nbsp;Из&nbsp;своего&nbsp;интернет&#8209;банка
+              </NavLink>
+            </div>
+          )}
+        />
+      </div>
     );
   }
 }
