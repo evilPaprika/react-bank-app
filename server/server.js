100399c7f67b9c0cec481bb9d6062cdc395b5ba2
evilpaprika <evilpaprika@localhost.localdomain>
2019-01-26 13:03:32 +0500

    ддос защита

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
+var Ddos = require("ddos");
 const express = require("express");
+var ddos = new Ddos({ burst: 30, limit: 50 });
+
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(ddos.express);
 const port = process.env.PORT || 5000;
 mongoose.connect(
     "mongodb+srv://bobross:bobross@bank-app-cgofe.mongodb.net/payments-test?retryWrites=true"
 );
 app.use(function(req, res, next) {
     console.log(
-        (req && req.method) + ":\n" + (req && req.body) + " " + req.route
+        (req && req.method) +
+            ":\n" +
+            (req && req.body) +
+            " " +
+            (req && req.route)
     );
     next();
 });
